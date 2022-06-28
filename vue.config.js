cf9c8a456772e2422c2d21794bba43ae591e6bbd
yezhengqi <320166202@qq.com>
2022-06-29 02:00:14 +0800

    更新了vue.confi.js文件

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  productionSourceMap: false,
+  // productionSourceMap: false,
   devServer: {  //代理解决跨域问题
     proxy: {  
       '/api':{  //当请求地址为api开头地址时，代理服务器向target发送请求并返回结果给本地服务器
-        target: 'http://gmall-h5-api.atguigu.cn'
+        target: 'http://gmall-h5-api.atguigu.cn',
+        changeOrigin: true
       },
-    }
+    },
   },
   publicPath:'./'
 })
